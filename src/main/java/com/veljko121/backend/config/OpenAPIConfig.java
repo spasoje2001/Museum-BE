b66fba101edfdb7d2e227ed227867cbceb17b7ad
dokma11 <vule.dok@gmail.com>
2024-03-26 21:16:16 +0100

    chore: Database setup

--- src/main/java/com/veljko121/backend/config/OpenAPIConfig.java
+++ src/main/java/com/veljko121/backend/config/OpenAPIConfig.java
@@ -23,7 +23,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
         servers = {
                 @Server(
                         description = "development server",
-                        url = "http://localhost:8080/"
+                        url = "http://localhost:8081/"
                 )
         },
         security = {
