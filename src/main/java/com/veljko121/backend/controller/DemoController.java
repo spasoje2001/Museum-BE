85c06463558234deb310fd0447b8cfaa58d1b7f8
Veljko Nikolić <nikolicveljko01@gmail.com>
2024-04-01 14:13:30 +0200

    Updated PreAuthorize

--- src/main/java/com/veljko121/backend/controller/DemoController.java
+++ src/main/java/com/veljko121/backend/controller/DemoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     
     @GetMapping
-    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('GUEST')")
     public String hello() {
         return "Hello!";
     }
