6f8cd36d46ab426ebde2baf4e2aa02dd5d20b35b
kuzminacc <kuzminacn@gmail.com>
2024-04-06 11:33:58 +0200

    fix: adding value to enum and changing sql script

--- src/main/java/com/veljko121/backend/dto/RegisterRequestDTO.java
+++ src/main/java/com/veljko121/backend/dto/RegisterRequestDTO.java
@@ -23,7 +23,6 @@ public class RegisterRequestDTO {
 
     private String lastName;
 
-    @NotEmpty
     private Role role;
     
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,14 @@ INSERT INTO _user(email, username, password, first_name, last_name, role) VALUES
     ('nikolicveljko01@gmail.com', 'veljko', '$2a$10$kVZ6MkAdppKk5GtbzjsgbubBx1Uos0EOTdiY9mIBA5XwWy1ezVQam', 'Veljko', 'Nikolić', 0),
     ('vule.dok@gmail.com', 'dokma', '$2a$10$jicdgzoNyODhfwtEuXsYHelIRjzFZ5zIgqcXlHfoy9BROPAnWV2y2', 'Vukašin', 'Dokmanović', 0),
     ('spale01@gmail.com', 'spale', '$2a$10$NhX4GkvqpZTas4mZa9wxuevS.8VCi70t3tKXVekdD5/VCg2Hf.kAu', 'Spasoje', 'Brborić', 0),
-    ('kuzminacn@gmail.com', 'nina', '$2a$10$XxmC.cIEK6XvYUVOa2HvTOlMQlIk0J6oQYouW2iBdgob.sRv78W9C', 'Nina', 'Kuzminac', 0);
+    ('kuzminacn@gmail.com', 'nina', '$2a$10$XxmC.cIEK6XvYUVOa2HvTOlMQlIk0J6oQYouW2iBdgob.sRv78W9C', 'Nina', 'Kuzminac', 0),
+    ('admin@gmail.com', 'admin', '$2a$12$zSJwNzNr/uFw.a9H5YRus.gQhZ.hjaNxhads7618x/U8KTFCPUnde', 'Admin', 'Adminovski', 4);
 
 INSERT INTO guest(id, biography) VALUES
     (1, 'Passionate about ancient civilizations, always seeking to uncover the mysteries of the past within museum walls..'),
     (2, 'I am interested in narco cartel history.'),
     (3, 'I am passionate about unraveling ancient civilizations and love exploring museums for insights.'),
     (4, 'Dedicated to preserving cultural heritage, advocating for the importance of museums in safeguarding our collective history.');
+
+INSERT INTO administrator(id, biography) VALUES
+    (5, 'Dedicated to preserving everything right as admin.');
