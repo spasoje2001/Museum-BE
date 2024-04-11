a9596b3b83b14f1bf4494b56811c2b42b297ceed
dokma11 <vule.dok@gmail.com>
2024-04-11 19:10:57 +0200

    feat: Personal Tour Request Service implementation

---
+++ src/main/java/com/veljko121/backend/service/IPersonalTourRequestService.java
@@ -0,0 +1,8 @@
+package com.veljko121.backend.service;
+
+import com.veljko121.backend.core.service.ICRUDService;
+import com.veljko121.backend.model.PersonalTourRequest;
+
+public interface IPersonalTourRequestService extends ICRUDService<PersonalTourRequest, Integer> {
+
+}
---
+++ src/main/java/com/veljko121/backend/service/impl/PersonalTourRequestService.java
@@ -0,0 +1,27 @@
+package com.veljko121.backend.service.impl;
+
+import com.veljko121.backend.core.service.impl.CRUDService;
+import com.veljko121.backend.model.PersonalTourRequest;
+import com.veljko121.backend.repository.PersonalTourRequestRepository;
+import com.veljko121.backend.service.IPersonalTourRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonalTourRequestService extends CRUDService<PersonalTourRequest, Integer> implements IPersonalTourRequestService{
+
+    @Autowired
+    private final PersonalTourRequestRepository personalTourRequestRepository;
+
+    public PersonalTourRequestService(JpaRepository<PersonalTourRequest, Integer> repository,
+                                      PersonalTourRequestRepository personalTourRequestRepository) {
+        super(repository);
+        this.personalTourRequestRepository = personalTourRequestRepository;
+    }
+
+    @Override
+    public PersonalTourRequest save(PersonalTourRequest request) {
+        return personalTourRequestRepository.save(request);
+    }
+}
