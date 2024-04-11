735aded843fdbebf51a475df37d6708a0f87bbce
Veljko Nikolić <nikolicveljko01@gmail.com>
2024-04-11 16:54:25 +0200

    Added all user role accounts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,13 @@
+-- var password = username;
 INSERT INTO _user(email, username, password, first_name, last_name, role) VALUES
     ('nikolicveljko01@gmail.com', 'veljko', '$2a$10$kVZ6MkAdppKk5GtbzjsgbubBx1Uos0EOTdiY9mIBA5XwWy1ezVQam', 'Veljko', 'Nikolić', 0),
     ('vule.dok@gmail.com', 'dokma', '$2a$10$jicdgzoNyODhfwtEuXsYHelIRjzFZ5zIgqcXlHfoy9BROPAnWV2y2', 'Vukašin', 'Dokmanović', 0),
     ('spale01@gmail.com', 'spale', '$2a$10$NhX4GkvqpZTas4mZa9wxuevS.8VCi70t3tKXVekdD5/VCg2Hf.kAu', 'Spasoje', 'Brborić', 0),
     ('kuzminacn@gmail.com', 'nina', '$2a$10$XxmC.cIEK6XvYUVOa2HvTOlMQlIk0J6oQYouW2iBdgob.sRv78W9C', 'Nina', 'Kuzminac', 0),
-    ('admin@gmail.com', 'admin', '$2a$12$zSJwNzNr/uFw.a9H5YRus.gQhZ.hjaNxhads7618x/U8KTFCPUnde', 'Admin', 'Adminovski', 4);
+    ('admin@gmail.com', 'admin', '$2a$12$zSJwNzNr/uFw.a9H5YRus.gQhZ.hjaNxhads7618x/U8KTFCPUnde', 'Admin', 'Adminovski', 4),
+    ('organizer@gmail.com', 'organizer', '$2a$10$KVAfGs.glFRaekHlH3OWaeUASPrUMoh7MBuNu8DYT/SzRnvD9ZjL6', 'Organizator', 'Organizatorović', 1),
+    ('curator@gmail.com', 'curator', '$2a$10$JGckcPY5eO8/1.xE7h7KmuH0A4gNif6PDSxBzUbxsThjQTTu.Nyai', 'Kustos', 'Kustosović', 2),
+    ('restaurateur@gmail.com', 'restaurateur', '$2a$10$tdoK34QObn2cUrANXLUzveWSSdkMuaT/EkN4RCzqmIjaAH0ze8PNW', 'Restaurator', 'Restaruatorović', 3);
 
 INSERT INTO guest(id, biography) VALUES
     (1, 'Passionate about ancient civilizations, always seeking to uncover the mysteries of the past within museum walls..'),
@@ -13,3 +17,12 @@ INSERT INTO guest(id, biography) VALUES
 
 INSERT INTO administrator(id, biography) VALUES
     (5, 'Dedicated to preserving everything right as admin.');
+
+INSERT INTO organizer(id, biography) VALUES
+    (6, 'I love organizing stuff.');
+
+INSERT INTO curator(id, biography) VALUES
+    (7, 'I love artifacts.');
+    
+INSERT INTO restaurateur(id, biography) VALUES
+    (8, 'I love restoring stuff.');
