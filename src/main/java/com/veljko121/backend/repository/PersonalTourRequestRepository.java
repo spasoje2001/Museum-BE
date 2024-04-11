bc995be6284660783842397b516a056289aa5731
dokma11 <vule.dok@gmail.com>
2024-04-11 19:10:40 +0200

    feat: Personal Tour Request Repository implementation

---
+++ src/main/java/com/veljko121/backend/repository/PersonalTourRequestRepository.java
@@ -0,0 +1,8 @@
+package com.veljko121.backend.repository;
+
+import com.veljko121.backend.model.PersonalTourRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonalTourRequestRepository extends JpaRepository<PersonalTourRequest, Integer> {
+
+}
