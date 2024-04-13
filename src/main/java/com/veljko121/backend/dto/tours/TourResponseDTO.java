4b46b1fb37e1b4060fdd96c7b5a60cd823cdbc27
dokma11 <vule.dok@gmail.com>
2024-04-13 19:17:24 +0200

    feat: TourResponseDTO implementation

---
+++ src/main/java/com/veljko121/backend/dto/tours/TourResponseDTO.java
@@ -0,0 +1,33 @@
+package com.veljko121.backend.dto.tours;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class TourResponseDTO {
+
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    //private List<Integer> roomIds = new ArrayList<>();
+
+    private String duration;
+
+    private LocalDateTime occurrenceDateTime;
+
+    private String adultTicketPrice;
+
+    private String minorTicketPrice;
+
+    private Integer guideId;
+
+    private String capacity;
+
+    private String picturePath;
+}
