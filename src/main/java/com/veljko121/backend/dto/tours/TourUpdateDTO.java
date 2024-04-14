b53c0456542e44038f26903da3fcdcbf1bfa5f58
dokma11 <vule.dok@gmail.com>
2024-04-14 11:58:11 +0200

    feat: TourUpdateDTO implementation

---
+++ src/main/java/com/veljko121/backend/dto/tours/TourUpdateDTO.java
@@ -0,0 +1,31 @@
+package com.veljko121.backend.dto.tours;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class TourUpdateDTO {
+
+    private String name;
+
+    private String description;
+
+    //private List<Integer> roomIds = new ArrayList<>();
+
+    private String duration;
+
+    private LocalDateTime occurrenceDateTime;
+
+    private String adultTicketPrice;
+
+    private String minorTicketPrice;
+
+    private Integer guideId;
+
+    private String capacity;
+
+    private String picturePath;
+}
