cbb9232ebd47c2926e34fcbc55488b6b88dade8c
dokma11 <vule.dok@gmail.com>
2024-05-16 19:02:09 +0200

    fix: Fix typo in jointable name

--- src/main/java/com/veljko121/backend/model/tours/PersonalTourRequest.java
+++ src/main/java/com/veljko121/backend/model/tours/PersonalTourRequest.java
@@ -32,7 +32,7 @@ public class PersonalTourRequest {
     private String guestNumber;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE})
-    @JoinTable(name = "peronal_tour_requests_exhibitions",
+    @JoinTable(name = "personal_tour_requests_exhibitions",
             joinColumns = @JoinColumn(name = "personal_tour_request_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "exhibition_id", referencedColumnName = "id"))
     @ToString.Exclude
